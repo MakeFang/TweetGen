57437b2cda86d2a854447164520b2819a3fb6fe2
Kuan-Ying Fang <kuanyingfang@gmail.com>
2018-10-30 11:53:38 -0700

    fixed the histogram method for list and tuples

--- tut_3/word_frequency.py
+++ tut_3/word_frequency.py
@@ -1,4 +1,10 @@
-import sys
+"""This script generates histograms of word counts for a text.
+
+This script uses three different data structures: dict, list of lists, and list
+of tuples. It also provides sorting by key and val.
+"""
+
+# import sys
 
 
 def read_text():
@@ -14,11 +20,13 @@ def read_text():
 
 
 def text_preprocessing(source_text):
+    """Process the input text."""
     return source_text.split()
 
 
-def histogram_dict(source_text):
-    word_list = text_preprocessing(source_text)
+def histogram_dict(word_list):
+    """Generate a histogram for word counts using dict."""
+    # word_list = text_preprocessing(source_text)
     hist_dict = {}
     for i in word_list:
         if i in hist_dict:
@@ -41,13 +49,30 @@ def histogram_dict(source_text):
 #     return histogram
 
 
-def histogram_ll(source_text):
-    hist_dict = histogram_dict(source_text)
-    histogram = []
-    for key, value in hist_dict.items():
-        histogram.append([key, value])
-    return histogram
+# def histogram_ll(source_text):
+#     hist_dict = histogram_dict(source_text)
+#     histogram = []
+#     for key, value in hist_dict.items():
+#         histogram.append([key, value])
+#     return histogram
 
+def histogram_ll(word_list):
+    """Generate a histogram for word counts using list of lists.
+
+    This function sorts the words first, and then run through the sorted lists
+    to populate the histogram.
+    """
+    # word_list = text_preprocessing(source_text)
+    word_list.sort()
+    histogram = []
+    currentword = None
+    for text_word in word_list:
+        if text_word == currentword:
+            histogram[-1][1] += 1
+        else:
+            histogram.append([text_word, 1])
+            currentword = text_word
+    return histogram
 
 # def histogram_lt(source_text):
 #     word_list = text_preprocessing(source_text)
@@ -62,19 +87,40 @@ def histogram_ll(source_text):
 #     return histogram
 
 
-def histogram_lt(source_text):
-    hist_dict = histogram_dict(source_text)
+# def histogram_lt(source_text):
+#     hist_dict = histogram_dict(source_text)
+#     histogram = []
+#     for key, value in hist_dict.items():
+#         histogram.append((key, value))
+#     return histogram
+
+
+def histogram_lt(word_list):
+    """Generate a histogram for word counts using list of tuple.
+
+    This function sorts the words first, and then run through the sorted lists
+    to populate the histogram.
+    """
+    # word_list = text_preprocessing(source_text)
+    word_list.sort()
     histogram = []
-    for key, value in hist_dict.items():
-        histogram.append((key, value))
+    currentword = None
+    for text_word in word_list:
+        if text_word == currentword:
+            histogram[-1] = (text_word, histogram[-1][1]+1)
+        else:
+            histogram.append([text_word, 1])
+            currentword = text_word
     return histogram
 
 
 def unique_words(histogram):
+    """Count the number of unique words in the text."""
     return len(histogram)
 
 
 def frequency(word, histogram):
+    """Count the occurance of a word in the text."""
     if type(histogram) == dict:
         if word in histogram:
             return histogram[word]
@@ -88,6 +134,7 @@ def frequency(word, histogram):
 
 
 def sort_hist_val(histogram):
+    """Sort the histogram by the counts, ascending."""
     if type(histogram) == dict:
         return sorted(histogram.items(), key=lambda x: x[1])
     elif type(histogram) == list:
@@ -96,6 +143,7 @@ def sort_hist_val(histogram):
 
 
 def sort_hist_key(histogram):
+    """Sort the histogram by the keys, alphabetically."""
     if type(histogram) == dict:
         return sorted(histogram.items(), key=lambda x: x[0])
     elif type(histogram) == list:
@@ -104,4 +152,8 @@ def sort_hist_key(histogram):
 
 
 if __name__ == '__main__':
-    print(histogram_lt(read_text()))
+    source_text = read_text()
+    word_list = text_preprocessing(source_text)
+    print(sort_hist_val(histogram_ll(word_list))[-20:])
+    print(sort_hist_val(histogram_lt(word_list))[-20:])
+    print(sort_hist_val(histogram_dict(word_list))[-20:])
